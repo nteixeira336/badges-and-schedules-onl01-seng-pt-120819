ea972344f27042386d7b6fdda02f029252b0df88
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-17 00:00:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def batch_badge_creator (names) 
+name= ["Edsger","Ada", "Charles","Alan","Grace","Linus","Matz"]
+count=0 
+while count <= names.length-1 
+puts "Hello, #{name}! You'll be assigned to room #{room}!"
+
+
+end
+
+
+end 
