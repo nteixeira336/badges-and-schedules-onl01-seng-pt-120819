8fb019e81902225f331a2221b5de2624d8f753a6
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-17 00:14:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,8 @@
-require 'pry'
 # Write your code here.
 def batch_badge_creator (name) 
 count=0 
 while count< name.length do 
   puts "Hello, #{name}! You'll be assigned to room #{rooms}!"
 end
-binding.pry 
 batch_badge_creator
-
 end 
