5fa567f110507c4cda1bbb3be8bd05a76fac786f
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-17 00:36:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def batch_badge_creator (speakers)
 names = [ ]
 count=0 
 while count< speakers.length do 
-  "Hello, #{speakers}!" 
+  names= "Hello, #{speakers}!" 
  speakers+=1 
 end
 batch_badge_creator
