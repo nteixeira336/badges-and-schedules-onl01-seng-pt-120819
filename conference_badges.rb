fbc087904f5d49ddd1f1c720af16e7ee692bb6ab
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-17 00:03:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,7 @@ def batch_badge_creator (name)
 name= ["Edsger","Ada", "Charles","Alan","Grace","Linus","Matz"]
 name.each_with_index do 
  puts "Hello, #{name}! You'll be assigned to room #{room}!"
-end
 binding.pry 
+end
+
 end 
