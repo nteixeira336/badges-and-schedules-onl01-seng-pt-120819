111256fa63c7e51ed6d85cb673bea7bb793bceed
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-17 00:09:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,5 @@
 # Write your code here.
 def badge_maker (name) 
-name= ["Edsger","Ada", "Charles","Alan","Grace","Linus","Matz"]
 count=0 
 while count< name.length do 
   puts "Hello, #{name}! You'll be assigned to room #{rooms}!"
